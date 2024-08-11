094abe06c8f8c9a4c4ab99b712c3b43bae2f667b
CJB <chris.berno@gmail.com>
2024-08-11 14:59:24 -0400

    removed react-dom import ! /empty-state/index.tsx

--- pages/empty-state/index.tsx
+++ pages/empty-state/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { Theme } from "@twilio-paste/core/theme";
 import { Box } from "@twilio-paste/core/box";
 import { Card } from "@twilio-paste/core/card";
